ab6253b41d6d25630ba8bc5fd651b691aa56b0a2
sangavimeenu123 <42232470+sangavimeenu123@users.noreply.github.com>
2018-08-28 13:56:06 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Solution {
+    public static void main(String args[] ) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        int N = Integer.parseInt(line);
+        for (int i = 0; i < N; i++) {
+            System.out.println("hello");
+        }
+    }
+}

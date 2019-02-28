c66a095106961405dee0fdffcb52685b6ce17920
sangavimeenu123 <42232470+sangavimeenu123@users.noreply.github.com>
2019-02-28 11:07:56 +0530

    Update alp.c

--- alp.c
+++ alp.c
@@ -4,7 +4,6 @@ int main()
     char c;
     printf("Enter a character: ");
     scanf("%c",&c);
-
     if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
         printf("%c is an alphabet.",c);
     else

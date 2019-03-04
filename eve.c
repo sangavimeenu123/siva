8f1d74b60ae2667e7239d0475c025408c41b2861
sangavimeenu123 <42232470+sangavimeenu123@users.noreply.github.com>
2019-03-04 14:16:30 +0530

    Update eve.c

--- eve.c
+++ eve.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int main()
 {
-    int n, i, flag = 0;
-    printf("Enter a positive integer: ");
-    scanf("%d", &n);
-    for(i = 2; i <= n/2; ++i)
-    {
-      if(n%i == 0)
-        {
-            flag = 1;
-            break;
-        }
+  int n, i, flag = 0;
+  printf("Enter a positive integer: ");
+  scanf("%d", &n);
+   for(i = 2; i <= n/2; ++i)
+   {
+     if(n%i == 0)
+       {
+         flag = 1;
+         break;
+       }
     }
     if (n == 1) 
     {

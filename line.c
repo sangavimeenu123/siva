b0c20f9127459f9a7d186639326adfea1a759445
sangavimeenu123 <42232470+sangavimeenu123@users.noreply.github.com>
2019-03-07 10:12:46 +0530

    Create line.c

---
+++ line.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#define MAX_SIZE 100 // Maximum string size
+
+int main()
+{
+    char str[MAX_SIZE];
+    int i, words;
+    printf("Enter any string: ");
+    gets(str);
+    i = 0;
+    words = 1;
+    while(str[i] != '\0')
+    {
+      if(str[i]==' ' || str[i]=='\n' || str[i]=='\t')
+        {
+            words++;
+        }
+
+        i++;
+    }
+    printf("Total number of words = %d", words);
+    return 0;
+}
